e7ac5c3a7b11aec2aac22a3df722abee6a16fcf3
jelly9 <xgd9543@gmail.com>
2016-10-31 23:56:21 +0800

    seqlist

--- seqlist.c
+++ seqlist.c
@@ -1,7 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
 
+#pragma warning(disable:4996) 
+#define MAXSIZE 200
+typedef int ElemType;
+
+typedef struct seqlist
+{
+	ElemType *data;
+	int listlength;
+	int listsize;
+}seqlist;
+void initlist( seqlist *L )
+{
+	L->data = ( ElemType* )malloc( MAXSIZE*sizeof(ElemType) );
+	L->listlength = 0;
+	L->listsize = MAXSIZE;
+}
+void insertelem(seqlist *L, int pos, ElemType e )
+{
+	int j = 0;
+	for( j = L->listlength+1; j>pos; j--)
+	{
+		L->data[j] = L->data[j-1];
+	}
+	L->data[pos-1] = e;
+	L->listlength++;
+}
+void printlist(seqlist *L)
+{
+	int i = 0;
+	for( i = 0; i<L->listlength; i++)
+	{
+		printf("%d ", L->data[i]);
+	}
+	printf("\n");
+}
 int main()
 {
-	printf("bit!\n");
+	seqlist L;
+	int i = 0;
+	int n = 0;
+	int len = 0;
+	initlist(&L);
+	printf("Please input length >: ");
+	scanf("%d", &len);
+	printf("Please input elem >:");
+	for( i = 1; i<len+1; i++)
+	{
+		scanf("%d",&n);
+		insertelem(&L,i,n);
+	}
+	printlist(&L);
+
+	system("pause");
 	return 0;
 }
