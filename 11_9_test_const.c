1ecb4901f7ac9df2e34e23073d6dddf4ca9e8c72
jelly9 <xgd9543@gmail.com>
2016-11-09 17:55:25 +0800

    const

---
+++ 11_9_test_const.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+	//const int a = 10;
+	//int b = 20;
+	//const int **pa;//
+	//int *const *pa ;//
+	//int ** const pa; //
+	//const int *const pa = &a;
+	//pa = &b;
+	//*pa = 20;
+
+
+	
+
+
+	system("pause");
+	return 0;
+}
